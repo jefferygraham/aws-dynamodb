a6d413781e8d7c2dcb4a4722ed2814ae69fb346b
jeffery graham <jeffery.lemar.graham@gmail.com>
2021-01-13 11:38:03 -0500

    api routes pt 2

--- td-notes-app/routes/appRoutes.js
+++ td-notes-app/routes/appRoutes.js
@@ -75,4 +75,90 @@ router.patch('/api/note', (req, res, next) => {
   );
 });
 
+router.get('/api/notes', (req, res, next) => {
+  let limit = req.query.limit ? parseInt(req.query.limit) : 3;
+  let params = {
+    TableName: tableName,
+    KeyConditionExpression: 'user_id = :uid',
+    ExpressionAttributeValues: {
+      ':uid': user_id,
+    },
+    Limit: limit,
+    ScanIndexForward: false,
+  };
+
+  let startTimestamp = req.query.start ? parseInt(req.query.start) : 0;
+
+  if (startTimestamp > 0) {
+    params.ExclusiveStartKey = {
+      user_id: user_id,
+      timestamp: startTimestamp,
+    };
+  }
+
+  docClient.query(params, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(err.statusCode).send({
+        message: err.message,
+        status: err.statusCode,
+      });
+    } else {
+      return res.status(200).send(data);
+    }
+  });
+});
+
+router.get('/api/notes/:note_id', (req, res, next) => {
+  let note_id = req.params.note_id;
+  let params = {
+    TableName: tableName,
+    IndexName: 'note_id-index',
+    KeyConditionExpression: 'note_id = :note_id',
+    ExpressionAttributeValues: {
+      ':note_id': note_id,
+    },
+    Limit: 1,
+  };
+
+  docClient.query(params, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(err.statusCode).send({
+        message: err.message,
+        status: err.statusCode,
+      });
+    } else {
+      if (!_.isEmpty(data.Items)) {
+        return res.status(200).send(data.Items[0]);
+      } else {
+        return res.status(404).send();
+      }
+    }
+  });
+});
+
+router.delete('/api/notes/:timestamp', (req, res, next) => {
+  let timestamp = parseInt(req.params.timestamp);
+  let params = {
+    TableName: tableName,
+    Key: {
+      user_id: user_id,
+      timestamp: timestamp,
+    },
+  };
+
+  docClient.delete(params, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(err.statusCode).send({
+        message: err.message,
+        status: err.statusCode,
+      });
+    } else {
+      return res.status(200).send();
+    }
+  });
+});
+
 module.exports = router;
